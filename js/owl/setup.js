cbaee216ccc1fc96f116eb35b875fce8c59f6fcb
Gabriela Beatriz <dibellatriz@gmail.com>
2022-01-01 16:55:57 -0300

    Primeiro commit

---
+++ js/owl/setup.js
@@ -0,0 +1,41 @@
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:false,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:5
+        }
+    }
+})
+
+
+
+$(document).ready(function() {
+
+    $('a[name=modal]').click(function(e) {
+        e.preventDefault();
+        var id = $(this).attr('href');
+        var maskHeight = $(document).height();
+        $('#mask').css({ 'height': maskHeight });
+        $('#mask').fadeIn(1000);
+        $('#mask').fadeTo("slow", 0.8);
+        $(id).fadeIn(2000);
+    });
+
+    $('.window .close').click(function(e) {
+        e.preventDefault();
+        $('#mask, .window').hide();
+    });
+
+    $('#mask').click(function() {
+        $(this).hide();
+        $('.window').hide();
+    });
+});
